63927b6b8af3da98ef958684a1e60936e9396dfe
Luis <luis.willnat.lw@gmail.com>
2017-02-03 13:11:33 +0100

    MongoDB & Get Data
    Added first method to get data from database

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,49 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var parser = require("body-parser");
+var mongo = require('mongodb').MongoClient;
+var MongoObjectID = require('mongodb').ObjectID;
 
-var fs = require('fs');
-var dataHome = require('../weatherData/homeData.json');
+var idToFind = '5893d692663c25f41f2f24c5';
+var objToFind = {_id: new MongoObjectID(idToFind)};
+
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+
+mongo.connect(url);
 
 module.exports = router;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',
-      { title: 'Express',
-        home: dataHome
-      });
-});
-
-
-router.use(parser.urlencoded({extended: true}));
-
-router.post("/", function (req, res){
-    increaseTemp();
+router.get('/', function(req, res, next){
+    res.render('index', {title: "Index", home: 0});
 });
 
 
 
-function increaseTemp() {
-    newValue = 35;
+router.get('/get-data', function(req, res, next) {
+    var resultArray = [];
+    mongo.connect(url, function(err, db){
+        var cursor = db.collection("data").find();
 
-    var fileName = '../weatherData/homeData.json';
-    var file = require(fileName) ;
+        cursor.forEach(function(doc, err){
+                assert.equal(null, err);
+                resultArray.push(doc);
+            }, function(){
+                console.log(resultArray[0].temp);
+                db.close();
+                res.render('index', {home: resultArray[0].temp, title: "Index"});
+            }
+        );
+    })
+});
 
-    file.temp = newValue;
 
 
-    console.log("success");
 
-    fs.writeFile(fileName, JSON.stringify(file), function(err){
-        if (err) return console.log(err);
-        console.log(JSON.stringify(file));
-        console.log('writing to ' + fileName);
-    });
-    fs.close(file);
-}
+
+
 
 
 
