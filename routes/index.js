45638feb41ca4ce019eff802030c1f9287099bef
PascalFickert <pascal.fickert@googlemail.com>
2017-02-08 19:47:26 +0100

    Indentation de index.ejs, variables plus lisibles index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
-var bodyParser = require("body-parser"); // body-parser extract data from the <form> and add it to the body property in the request object.
-var mongo = require('mongodb').MongoClient;
-var objectID = require('mongodb').ObjectID;
-var assert = require('assert');
-var passport = require('passport');
-var weatherAPI = require('openweather-apis');
+var express     = require('express');
+var router      = express.Router();
+var request     = require('request');
+var bodyParser  = require("body-parser"); // body-parser extract data from the <form> and add it to the body property in the request object.
+var mongo       = require('mongodb').MongoClient;
+var objectID    = require('mongodb').ObjectID;
+var assert      = require('assert');
+var passport    = require('passport');
+var idHome      = '58946479f12654d263d62844'; //id of our home, doesn't change, it's a constant
+var url         = 'mongodb://localhost:27017/test';
+var einheit     = "°C";
+var weatherAPI  = require('openweather-apis');
 weatherAPI.setAPPID('1c12a784ad25f95111035d8132662635');
 weatherAPI.setCity('Saarbruecken');
-var idHome = '58946479f12654d263d62844'; //id of our home, doesn't change, it's a constant
-var url = 'mongodb://localhost:27017/test';
-var einheit = "°C";
+
 router.use(bodyParser.urlencoded({extended: true})); // otherwise get the error : "bodyparser is outdated"
 
 module.exports = router;
